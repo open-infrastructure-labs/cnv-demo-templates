30282ef5dc836f343d4666f4308aa1842b639358
Lars Kellogg-Stedman <lars@redhat.com>
2020-08-28 10:08:24 -0400

    add playbook for uploading images to minio

--- upload-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-virtctl image-upload dv centos-8-stream \
-  --image-path CentOS-Stream-GenericCloud-8-20200113.0.x86_64.qcow2 \
-  --size 10Gi \
-  --storage-class hostpath-provisioner \
-  --insecure
